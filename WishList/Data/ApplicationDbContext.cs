dd182a853e806786adb0260e23266ebee7a210b3
Kristian Bonitz <kbonitz@woolworths.com.au>
2021-07-17 07:53:54 +1000

    Getting started on the DB model with Wishlist Items

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbContextOptions _options;
+        public DbSet<Item> Items;
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+    }
+}
