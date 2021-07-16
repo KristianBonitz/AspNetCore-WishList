7b72ed39a550d14c35f8179c1eb7899cc4e30b56
Kristian Bonitz <kbonitz@woolworths.com.au>
2021-07-17 08:09:06 +1000

    Update ApplicationDbContext.cs

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         DbContextOptions _options;
-        public DbSet<Item> Items;
+        public DbSet<Item> Items { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
